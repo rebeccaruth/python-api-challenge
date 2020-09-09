aedfe67812a5d834177d4b3a7aa37feffcfd4ff2
Rebecca Ruth <Ruth0506@live.missouristate.edu>
2020-09-08 22:38:13 -0500

    dependencies

--- WeatherPy/api_keys.py
+++ WeatherPy/api_keys.py
@@ -1,5 +1,5 @@
 # OpenWeatherMap API Key
-weather_api_key = "YOUR KEY HERE!"
+weather_api_key = "eb5c71fc4a4b2030e18ff3aded7568bb"
 
 # Google API Key
-g_key = "YOUR KEY HERE!"
+g_key = "AIzaSyA85A3x-S09mTOPA0KBk9qG7iVQ-AggSzs"
